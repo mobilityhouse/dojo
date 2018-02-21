a6b4e3df5612177910b4771a5d2065266a58a18d
Münteha <munteha.yildiz@mobilityhouse.com>
2018-02-21 16:51:54 +0100

    End of first Dojo

--- 2018.02.21/stringcalculator/stringcalculator.py
+++ 2018.02.21/stringcalculator/stringcalculator.py
@@ -1,4 +1,17 @@
+import re
 
+def get_numbers(value, separator):
+    value = list(map(int, str.split(sep=separator)))
+    return sum(value)
 
-def method():
-    pass
+def get_separator(inpt):
+    return inpt[2:inpt.startswith(//)]
+
+def add(value):
+    if value == '':
+        return "0"
+    else:
+        return str(sum(get_numbers(value)))
+
+#num_input = [0]
+#result = sum(map(int,num_input))
--- 2018.02.21/tests/test_stringcalculator.py
+++ 2018.02.21/tests/test_stringcalculator.py
@@ -1,5 +1,18 @@
-from stringcalculator.stringcalculator import method
+from stringcalculator.stringcalculator import add, get_numbers
+import pytest
+
+@pytest.mark.parametrize('value, output', [
+    ('', '0'),
+    ('1', '1'),
+    ('1,2', '3'),
+    ('1,2,6', '9'),
+    ('1\n3\n4', '8'),
+    ('1\n,\n3', "Number expected but , found on position 3")
+])
+def test_add(value, output):
+    assert add(value) == output
 
 
-def test_method():
-    assert method() is not None
+def test_get_numbers():
+    assert get_numbers('1 ,2') == [1, 2]
+
