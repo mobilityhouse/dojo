d0a131778c5294fe81d9aa033e98c10e8b4e305c
JonathanHerrmann <jonathan.herrmann@mobilityhouse.com>
2018-03-08 16:16:05 +0100

    Add what we done so far

--- 2018.03.08/tennis/scoring.py
+++ 2018.03.08/tennis/scoring.py
@@ -1,10 +1,31 @@
 
 class Score:
-    def player_one_scores():
-        pass
+    p1_score = 0
+    p2_score = 0
 
-    def player_two_scores():
-        pass
+    score_names = {
+        0: 'love',
+        1: 'fifteen',
+        2: 'thirty',
+        3: 'forty',
+    }
 
-    def get_score():
-        return None
+    advantage = None
+
+    def player_one_scores(self):
+        if self.p1_score < 4:
+            self.p1_score += 1
+
+    def player_two_scores(self):
+        if self.p2_score < 4:
+            self.p2_score += 1
+
+    def get_score(self):
+        if self.p1_score + self.p2_score >= 6:
+            return 'Deuce'
+        if self.p1_score > 3:
+            return 'Player 1 wins!!!'
+        if self.p2_score > 3:
+            return 'Player 2 wins!!!'
+        return '{p1} x {p2}'.format(p1=self.score_names[self.p1_score],
+                                    p2=self.score_names[self.p2_score])
--- 2018.03.08/tests/test_score.py
+++ 2018.03.08/tests/test_score.py
@@ -1,5 +1,39 @@
+import pytest
+
 from tennis.scoring import Score
 
 
-def test_score():
-    assert None is Score.get_score()
+def test_initial_score():
+    score = Score()
+    assert score.get_score() == 'love x love'
+
+
+@pytest.mark.parametrize('play, expected_score', [
+    (1, 'fifteen x love'),
+    (2, 'love x fifteen'),
+])
+def test_a_player_scores(play, expected_score):
+    score = Score()
+    if play == 1:
+        score.player_one_scores()
+    else:
+        score.player_two_scores()
+    assert score.get_score() == expected_score
+
+
+@pytest.mark.parametrize('plays, expected_score', [
+    ([1, 1, 1], 'forty x love'),
+    ([2, 1, 2, 1],  'thirty x thirty'),
+    ([1, 1, 1, 1], 'Player 1 wins!!!'),
+    ([2, 2, 2, 2], 'Player 2 wins!!!'),
+    ([2, 2, 2, 1, 1, 1], 'Deuce'),
+    ([2, 2, 2, 1, 1, 1, 1], 'Advantage player 1'),
+])
+def test_multiple_scores(plays, expected_score):
+    score = Score()
+    for play in plays:
+        if play == 1:
+            score.player_one_scores()
+        else:
+            score.player_two_scores()
+    assert score.get_score() == expected_score
